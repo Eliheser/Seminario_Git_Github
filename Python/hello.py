ed482ed8884de5e5216b579e46c155d157b91f9e
Fernando Chicas <2546192023@mail.utec.edu.sv>
2023-10-31 17:53:57 -0600

    Tipo de datos, usando Type

---
+++ Python/hello.py
@@ -0,0 +1,16 @@
+#  Esto es un comnetario: Hola mundo
+# Nuestro Hola Mundo
+print("Hola Python")
+
+"""
+Este es un comentario 
+en varias lineas
+"""
+
+# Consultar tipo de datos
+
+print(type("Soy un dato str")) #Dato tipo str
+print(type(5)) #Soy un dato tipo Int
+print(type(1.5)) #Soy dato tipo float
+print(type(True)) #Soy dato tipo bool
+print(type(1 + 2j)) #tipo de dato Complex numbre
